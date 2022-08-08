5aafbbc035dd1133cb19518403b8c63c368ebc4d
Eduardo Dreyer <eduardo.dreyer@mt.senac.br>
2022-08-08 13:50:45 -0400

    first commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$hostname	= "mysql";
+$dbname		= "table";
+$username	= "eduardodreyer";
+$password	= "eduardodreyer";
+
+try {
+
+	$conn = new PDO( "mysql:host=$hostname;dbname=$dbname", $username, $password );
+
+	// Configure PDO error mode
+	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+	echo "Connected successfully";
+}
+catch( PDOException $e ) {
+
+	echo "Failed to connect: " . $e->getMessage();
+}
